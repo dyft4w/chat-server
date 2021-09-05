c7c7ddc62b138fbaccb46f58e7fa790dfbb7c913
dyft4w <dyft4w@gmail.com>
2021-09-05 14:35:31 +0200

    added an include

--- connection.hpp
+++ connection.hpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <thread>
 class Connection:public std::enable_shared_from_this<Connection>{
 public:
     static std::shared_ptr<Connection> create(asio::io_context& io_context, std::vector<std::string>& messages){
